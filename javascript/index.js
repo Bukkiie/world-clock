6a34da151813c4217ec04e6fac9608ebdd3de02f
Bukkiie <adebukolaabimbolaajayi@gmail.com>
2024-06-22 15:00:43 +0000

    Added interaction to my code (JS)

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+setInterval(function () {
+  // los angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+});
+
+setInterval(function () {
+  //sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Europe/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+});
